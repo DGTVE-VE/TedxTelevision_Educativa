f8273865585eab41880d6edb1cda3d9a27d3a672
Sonia Martinez <soniamartinezctr@gmail.com>
2016-09-26 17:33:53 -0500

    Crear controlador Integrar footer y header con su hoja de estilo

---
+++ resources/views/viewTedx/header.blade.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-Tedx navbar-fixed-top navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">TEDx</a>
+        </div>
+        <div class="collapse navbar-collapse" id="navbar">
+            <ul class="nav navbar-nav navbar-right ">
+                <li class="active"><a href="#">Inicio</a></li>
+                <li class=""><a href="#">Qué es TED</a></li>
+                <li class=""><a href="#">Programa</a></li>
+                <li class=""><a href="#">Ponentes</a></li>
+                <li class=""><a href="#">Organizadores</a></li>                                
+            </ul>
+        </div>
+    </div>
+</nav>
+<div class="navbar navbar-default"></div>

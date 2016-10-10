f8e0e09f31a2bb69b4e4dded24477b844ba65159
Crapaud <renecrapaud@gmail.com>
2016-10-10 13:51:48 -0500

    Sección colaboradores. Cambio de versión de JQuery en Index para hacer responsiva la imagen que contiene las áreas para foto de colaboradores.

--- resources/views/viewTedx/organizadores.blade.php
+++ resources/views/viewTedx/organizadores.blade.php
@@ -26,9 +26,9 @@
 			<map name="mapaOrganizadores" id="mapaOrganizadores">
 				<area alt="" title="" href="#" shape="poly" coords="487,590,529,538,547,540,550,520,558,522,558,501,565,483,584,474,594,474,605,484,617,496,621,514,620,551,648,570,639,599,622,606,614,622,623,635,627,649,623,664,620,680,590,734,579,642,507,638" onmouseover="muestraColaborador()" onmouseleave="ocultaColaborador()"/>
 			</map>
-            <img class="" src="{{asset('images/organizadores/organizadores.jpg')}}">
-			<img id="colaboradorResalta" class="imgEncima invisible" src="{{asset('images/organizadores/musicasCompleto.png')}}">
-			<img class="imgEncima" src="{{asset('images/organizadores/frenteOrganiza.png')}}" alt="" usemap="#mapaOrganizadores" />
+            <img class="img-responsive" src="{{asset('images/organizadores/organizadores.jpg')}}"/>
+			<img id="colaboradorResalta" class="imgEncima invisible img-responsive" src="{{asset('images/organizadores/musicasCompleto.png')}}"/>
+			<img class="imgEncima img-responsive" src="{{asset('images/organizadores/frenteOrganiza.png')}}" alt="" usemap="#mapaOrganizadores"/>
 		</div>
     </div>
 </section>
@@ -105,5 +105,6 @@
     /* ------    Se llama la función que redimensiona mapas de imagenes responsivas -----  */
     $(document).ready(function (e) {
         $('img[usemap]').rwdImageMaps();
+		console.log('ejecutado');
     });
 </script>

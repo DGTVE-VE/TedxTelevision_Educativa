e3bdbf5c61071f40e1e90a9354a96f7517990e59
Crapaud <renecrapaud@gmail.com>
2016-10-18 11:28:47 -0500

    Galeria flickr. Agregados marcos de reproducción de fotos.

--- resources/views/viewTedx/galeria.blade.php
+++ resources/views/viewTedx/galeria.blade.php
@@ -2,20 +2,21 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12 text-center textBlanco"><h3><strong>GALERIA</strong></h3></div>
-            <div class="col-md-5">
-                <object width="100%" height="300"> <param name="flashvars" value="offsite=true&lang=es-us&page_show_url=%2Fphotos%2F145504772%40N07%2Fshow%2F&page_show_back_url=%2Fphotos%2F145504772%40N07%2F&user_id=145504772@N07&jump_to="></param> <param name="movie" value="https://www.flickr.com/apps/slideshow/show.swf?v=261948265"></param> <param name="allowFullScreen" value="true"></param><embed type="application/x-shockwave-flash" src="https://www.flickr.com/apps/slideshow/show.swf?v=261948265" allowFullScreen="true" flashvars="offsite=true&lang=es-us&page_show_url=%2Fphotos%2F145504772%40N07%2Fshow%2F&page_show_back_url=%2Fphotos%2F145504772%40N07%2F&user_id=145504772@N07&jump_to=" width="110%" height="300"></embed></object>
-            </div>
             <div class="col-md-3">
-				<object width="100%" height="300"> <param name="flashvars" value="offsite=true&lang=es-us&page_show_url=%2Fphotos%2F145504772%40N07%2Fshow%2F&page_show_back_url=%2Fphotos%2F145504772%40N07%2F&user_id=145504772@N07&jump_to="></param> <param name="movie" value="https://www.flickr.com/apps/slideshow/show.swf?v=261948265"></param> <param name="allowFullScreen" value="true"></param><embed type="application/x-shockwave-flash" src="https://www.flickr.com/apps/slideshow/show.swf?v=261948265" allowFullScreen="true" flashvars="offsite=true&lang=es-us&page_show_url=%2Fphotos%2F145504772%40N07%2Fshow%2F&page_show_back_url=%2Fphotos%2F145504772%40N07%2F&user_id=145504772@N07&jump_to=" width="110%" height="300"></embed></object>
+				<a data-flickr-embed="true"  href="https://www.flickr.com/photos/145504772@N07/albums/72157671859908893" title="Angostas"><img src="https://c6.staticflickr.com/6/5776/30302490261_19e8d7ee2a_n.jpg" width="100%" height="300" alt="Angostas"></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
 			</div>
-            <div class="col-md-4">
-				<object width="100%" height="300"> <param name="flashvars" value="offsite=true&lang=es-us&page_show_url=%2Fphotos%2F145504772%40N07%2Fshow%2F&page_show_back_url=%2Fphotos%2F145504772%40N07%2F&user_id=145504772@N07&jump_to="></param> <param name="movie" value="https://www.flickr.com/apps/slideshow/show.swf?v=261948265"></param> <param name="allowFullScreen" value="true"></param><embed type="application/x-shockwave-flash" src="https://www.flickr.com/apps/slideshow/show.swf?v=261948265" allowFullScreen="true" flashvars="offsite=true&lang=es-us&page_show_url=%2Fphotos%2F145504772%40N07%2Fshow%2F&page_show_back_url=%2Fphotos%2F145504772%40N07%2F&user_id=145504772@N07&jump_to=" width="110%" height="300"></embed></object>
+			<div class="col-md-6">
+				<a data-flickr-embed="true"  href="https://www.flickr.com/photos/145504772@N07/albums/72157675376608895" title="Camara"><img src="https://c4.staticflickr.com/6/5483/30388487195_f4fbb11262.jpg" width="100%" height="300" alt="Camara"></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
 			</div>
-			<div class="col-md-7">
-				<object width="100%" height="300"> <param name="flashvars" value="offsite=true&lang=es-us&page_show_url=%2Fphotos%2F145504772%40N07%2Fshow%2F&page_show_back_url=%2Fphotos%2F145504772%40N07%2F&user_id=145504772@N07&jump_to="></param> <param name="movie" value="https://www.flickr.com/apps/slideshow/show.swf?v=261948265"></param> <param name="allowFullScreen" value="true"></param><embed type="application/x-shockwave-flash" src="https://www.flickr.com/apps/slideshow/show.swf?v=261948265" allowFullScreen="true" flashvars="offsite=true&lang=es-us&page_show_url=%2Fphotos%2F145504772%40N07%2Fshow%2F&page_show_back_url=%2Fphotos%2F145504772%40N07%2F&user_id=145504772@N07&jump_to=" width="110%" height="300"></embed></object>
+			<div class="col-md-3">			
+				<a data-flickr-embed="true"  href="https://www.flickr.com/photos/145504772@N07/albums/72157671860972263" title="perspectiva"><img src="https://c3.staticflickr.com/9/8554/29758138874_0d484af69f_n.jpg" width="100%" height="300" alt="perspectiva"></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
 			</div>
-			<div class="col-md-5">
-				<object width="100%" height="300"> <param name="flashvars" value="offsite=true&lang=es-us&page_show_url=%2Fphotos%2F145504772%40N07%2Fshow%2F&page_show_back_url=%2Fphotos%2F145504772%40N07%2F&user_id=145504772@N07&jump_to="></param> <param name="movie" value="https://www.flickr.com/apps/slideshow/show.swf?v=261948265"></param> <param name="allowFullScreen" value="true"></param><embed type="application/x-shockwave-flash" src="https://www.flickr.com/apps/slideshow/show.swf?v=261948265" allowFullScreen="true" flashvars="offsite=true&lang=es-us&page_show_url=%2Fphotos%2F145504772%40N07%2Fshow%2F&page_show_back_url=%2Fphotos%2F145504772%40N07%2F&user_id=145504772@N07&jump_to=" width="110%" height="300"></embed></object>
+			<div class="col-md-12"></div>
+			<div class="col-md-6">
+				<a data-flickr-embed="true"  href="https://www.flickr.com/photos/145504772@N07/albums/72157675280675096" title="Blanco y negro"><img src="https://c6.staticflickr.com/6/5160/29925345941_e254fbfda9.jpg" width="100%" height="333" alt="Blanco y negro"></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
+			</div>
+			<div class="col-md-6">
+				<a data-flickr-embed="true"  href="https://www.flickr.com/photos/145504772@N07/albums/72157671852177394" title="Colorido"><img src="https://c6.staticflickr.com/6/5486/29756830293_74dc07bf6f.jpg" width="100%" height="333" alt="Colorido"></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
 			</div>
         </div>
     </div>

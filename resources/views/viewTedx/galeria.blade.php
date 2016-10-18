7a79e4047fabd4e942ace35af6be40a0982ddfbf
Crapaud <renecrapaud@gmail.com>
2016-10-18 13:00:41 -0500

    Galeria flickr. Ajuste en las posiciones de los contenedores de fotos.

--- resources/views/viewTedx/galeria.blade.php
+++ resources/views/viewTedx/galeria.blade.php
@@ -1,22 +1,32 @@
+<style>
+	.posicionaIzq{
+		position: relative;
+		left: -5%;
+	}
+	.posicionaIzqSeg{
+		position: relative;
+		left: -10%;
+	}
+</style>
 <section id="galeria">
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
             <div class="col-md-12 text-center textBlanco"><h3><strong>GALERIA</strong></h3></div>
             <div class="col-md-3">
-				<a data-flickr-embed="true"  href="https://www.flickr.com/photos/145504772@N07/albums/72157671859908893" title="Angostas"><img src="https://c6.staticflickr.com/6/5776/30302490261_19e8d7ee2a_n.jpg" width="100%" height="300" alt="Angostas"></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
+				<a data-flickr-embed="true"  href="https://www.flickr.com/photos/145504772@N07/albums/72157671859908893" title="Angostas"><img src="https://c6.staticflickr.com/6/5776/30302490261_19e8d7ee2a_n.jpg" width="90%" alt="Angostas"></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
 			</div>
 			<div class="col-md-6">
-				<a data-flickr-embed="true"  href="https://www.flickr.com/photos/145504772@N07/albums/72157675376608895" title="Camara"><img src="https://c4.staticflickr.com/6/5483/30388487195_f4fbb11262.jpg" width="100%" height="300" alt="Camara"></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
+				<a data-flickr-embed="true" href="https://www.flickr.com/photos/145504772@N07/albums/72157675280675096" title="Blanco y negro"><img src="https://c6.staticflickr.com/6/5160/29925345941_e254fbfda9.jpg" class="posicionaIzq" width="103%" alt="Blanco y negro"></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
 			</div>
 			<div class="col-md-3">			
-				<a data-flickr-embed="true"  href="https://www.flickr.com/photos/145504772@N07/albums/72157671860972263" title="perspectiva"><img src="https://c3.staticflickr.com/9/8554/29758138874_0d484af69f_n.jpg" width="100%" height="300" alt="perspectiva"></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
+				<a data-flickr-embed="true"  href="https://www.flickr.com/photos/145504772@N07/albums/72157671860972263" title="perspectiva"><img src="https://c3.staticflickr.com/9/8554/29758138874_0d484af69f_n.jpg" class="posicionaIzq" width="95%" alt="perspectiva"></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
 			</div>
 			<div class="col-md-12"></div>
 			<div class="col-md-6">
-				<a data-flickr-embed="true"  href="https://www.flickr.com/photos/145504772@N07/albums/72157675280675096" title="Blanco y negro"><img src="https://c6.staticflickr.com/6/5160/29925345941_e254fbfda9.jpg" width="100%" height="333" alt="Blanco y negro"></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
+				<a data-flickr-embed="true"  href="https://www.flickr.com/photos/145504772@N07/albums/72157675376608895" title="Camara"><img src="https://c4.staticflickr.com/6/5483/30388487195_f4fbb11262.jpg" width="93%" alt="Camara"></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
 			</div>
 			<div class="col-md-6">
-				<a data-flickr-embed="true"  href="https://www.flickr.com/photos/145504772@N07/albums/72157671852177394" title="Colorido"><img src="https://c6.staticflickr.com/6/5486/29756830293_74dc07bf6f.jpg" width="100%" height="333" alt="Colorido"></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
+				<a data-flickr-embed="true"  href="https://www.flickr.com/photos/145504772@N07/albums/72157671852177394" title="Colorido"><img src="https://c6.staticflickr.com/6/5486/29756830293_74dc07bf6f.jpg" class="posicionaIzqSeg" width="105%" alt="Colorido"></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
 			</div>
         </div>
     </div>

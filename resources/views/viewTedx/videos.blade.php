310d1aec905551706b8396327ba26e90300cc63f
Sergio Calera Fortis <SCALERA@tveducativa.mx>
2016-12-05 16:39:00 -0600

    Se remplazan video por los de tedxTelevisioneducativa

--- resources/views/viewTedx/videos.blade.php
+++ resources/views/viewTedx/videos.blade.php
@@ -7,52 +7,52 @@
 						<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
 							<div class="cajaVideoCuerpo">
 								<div class="contenedor">
-									<iframe src="https://embed.ted.com/talks/lang/es/sir_ken_robinson_bring_on_the_revolution" width="360" height="220" frameborder="0" scrolling="no" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+									<iframe width="490" height="370" src="http://www.youtube.com/embed/7mIhL-QloA4" frameborder="0" allowfullscreen="allowfullscreen" data-link="http://www.youtube.com/watch?v=7mIhL-QloA4"></iframe>
 								</div>
 							</div>
-							<p class="col-md-12 bottom-align-text">TED talks<br>6,611,604 visualizaciones</p>
+							<p class="col-md-12 bottom-align-text">Nuevo modelo educativo ¿Para qué?<br>Javier Treviño Cantú</p>
 						</div>
 						<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
 							<div class="cajaVideoCuerpo">
 								<div class="contenedor">
-									<iframe src="https://embed.ted.com/talks/rita_pierson_every_kid_needs_a_champion" width="360" height="220" frameborder="0" scrolling="no" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+									<iframe width="490" height="370" src="http://www.youtube.com/embed/g2oEyaL7S6E" frameborder="0" allowfullscreen="allowfullscreen" data-link="http://www.youtube.com/watch?v=g2oEyaL7S6E"></iframe>
 								</div>
 							</div>
-							<p class="col-md-12 bottom-align-text">TED talks<br>6,189,600 visualizaciones</p>
+							<p class="col-md-12 bottom-align-text">Trabajo en equipo para salir de la montaña<br>Sergio Ramírez Zúñiga</p>
 						</div>
 						<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
 							<div class="cajaVideoCuerpo">
 								<div class="contenedor">
-									<iframe src="https://embed.ted.com/talks/lang/es/sugata_mitra_build_a_school_in_the_cloud" width="360" height="220" frameborder="0" scrolling="no" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+									<iframe width="490" height="370" src="http://www.youtube.com/embed/e_DCyDEcWNI" frameborder="0" allowfullscreen="allowfullscreen" data-link="http://www.youtube.com/watch?v=e_DCyDEcWNI"></iframe>
 								</div>
 							</div>
-							<p class="col-md-12 bottom-align-text">TED talks<br>2,671,931 visualizaciones</p>
+							<p class="col-md-12 bottom-align-text">Prohibido no tocar: museos a la inversa<br>Marinela Servitje Montull</p>
 						</div>
 					</li>
 					<li>
 						<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
 							<div class="cajaVideoCuerpo">
 								<div class="contenedor">
-									<iframe src="https://embed.ted.com/talks/angela_lee_duckworth_grit_the_power_of_passion_and_perseverance" width="360" height="220" frameborder="0" scrolling="no" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+									<iframe width="490" height="370" src="http://www.youtube.com/embed/J4NJQU5SMPM" frameborder="0" allowfullscreen="allowfullscreen" data-link="http://www.youtube.com/watch?v=J4NJQU5SMPM"></iframe>
 								</div>
 							</div>
-							<p class="col-md-12 bottom-align-text">TED talks<br>9,379,027 visualizaciones</p>
+							<p class="col-md-12 bottom-align-text">El zen de la administración del tiempo<br>Arie Schwartzman Faerman</p>
 						</div>
 						<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
 							<div class="cajaVideoCuerpo">
 								<div class="contenedor">
-									<iframe src="https://embed.ted.com/talks/lang/es/salman_khan_let_s_use_video_to_reinvent_education" width="360" height="220" frameborder="0" scrolling="no" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+									<iframe width="490" height="370" src="http://www.youtube.com/embed/YNaZ_xavZ-4" frameborder="0" allowfullscreen="allowfullscreen" data-link="http://www.youtube.com/watch?v=YNaZ_xavZ-4"></iframe>
 								</div>
 							</div>
-							<p class="col-md-12 bottom-align-text">TED talks<br>4,264,221 visualizaciones</p>
+							<p class="col-md-12 bottom-align-text">Profesión mata innovación...!Pare de sufrir!<br>Rubén Edel Navarro</p>
 						</div>
 						<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
 							<div class="cajaVideoCuerpo">
 								<div class="contenedor">
-									<iframe src="https://embed.ted.com/talks/elizabeth_gilbert_success_failure_and_the_drive_to_keep_creating" width="360" height="220" frameborder="0" scrolling="no" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+									<iframe width="490" height="370" src="http://www.youtube.com/embed/x3gsVA00kyA" frameborder="0" allowfullscreen="allowfullscreen" data-link="http://www.youtube.com/watch?v=x3gsVA00kyA"></iframe>
 								</div>
 							</div>
-							<p class="col-md-12 bottom-align-text">TED talks<br>3,079,938 visualizaciones</p>
+							<p class="col-md-12 bottom-align-text">La caja inteligente: una galaxia en el cerebro<br>Pepe Gordon</p>
 						</div>
 					</li>
 				</ul>
